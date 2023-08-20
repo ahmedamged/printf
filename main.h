eb0536005570cdf162f21e3d5749ac72b678f56e
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-21 01:56:06 +0300

    updated

--- main.h
+++ main.h
@@ -4,7 +4,4 @@
 #include <stddef.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
-int handle_switch(const char *format, va_list args, int old_count, unsigned int *i);
-int handle_char(char c, int old_count);
-int handle_string(char *s, int old_count);
 #endif
