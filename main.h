2ff1f5a5d48205949f7459745ca68e45a642a609
DJ-Man-2099 <davidmesak@hotmail.com>
2023-08-22 22:06:10 +0300

    updated

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdbool.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
 int handle_format(unsigned int *i, const char *format,
@@ -9,5 +10,8 @@ int handle_format(unsigned int *i, const char *format,
 int handle_char(char c, int old_count);
 int handle_string(char *s, int old_count);
 int handle_int(int i, int old_count);
+int handle_unsigned_int(unsigned int i, int old_count);
 int handle_int_to_binary(unsigned int i, int old_count);
+int handle_int_to_octal(unsigned int i, int old_count);
+int handle_int_to_hex(unsigned int i, int old_count, bool isCapital);
 #endif
